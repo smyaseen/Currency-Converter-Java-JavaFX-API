2fd672133f3a2db21db1fe1d89b35e565248ac14
smy <smyaseen164@gmail.com>
2021-01-29 17:23:46 +0500

    GUI implemented

--- src/org/smy/currencyConverter/Main.java
+++ src/org/smy/currencyConverter/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/Window.fxml"));
         primaryStage.setTitle("Currency Converter");
-        primaryStage.setScene(new Scene(root, 600, 480));
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
