389af91e62163fcf271a744b37a126ace1928bbb
smy <smyaseen164@gmail.com>
2021-01-30 13:05:37 +0500

    implemented all features, need to add more countries

--- src/module-info.java
+++ src/module-info.java
@@ -4,7 +4,6 @@ module Currency.Converter.Java.JavaFX.and.API {
     requires javafx.graphics;
     requires javafx.base;
     requires javafx.controls;
-
     opens org.smy.currencyConverter.controller;
     opens org.smy.currencyConverter;
     opens org.smy.currencyConverter.view;
--- src/org/smy/currencyConverter/controller/Controller.java
+++ src/org/smy/currencyConverter/controller/Controller.java
@@ -1,4 +1,96 @@
 package org.smy.currencyConverter.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.image.ImageView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Controller {
+
+    @FXML
+    private Label rate;
+
+    @FXML
+    private Spinner<Integer> amount;
+
+    @FXML
+    private ChoiceBox<String> convertFrom;
+
+
+    @FXML
+    private ChoiceBox<String> convertTo;
+
+    public void initialize() {
+        ArrayList<String> currencyList = new ArrayList<>();
+        currencyList.add("PKR");
+        currencyList.add("USD");
+
+        convertFrom.getItems().addAll(currencyList);
+        convertTo.getItems().addAll(currencyList);
+
+        convertFrom.getSelectionModel().select("USD");
+        convertTo.getSelectionModel().select("PKR");
+
+        amount.getValueFactory().setValue(1);
+
+    }
+
+    @FXML
+    public void reverseConversion() {
+
+        String convertToString = convertFrom.getSelectionModel().getSelectedItem();
+
+        convertFrom.getSelectionModel().select(
+                convertTo.getSelectionModel().getSelectedItem()
+        );
+
+        convertTo.getSelectionModel().select(convertToString);
+
+        convert();
+    }
+
+    @FXML
+    public void convert() {
+        rate.setText("" + getRate(
+                convertFrom.getSelectionModel().getSelectedItem(),
+                convertTo.getSelectionModel().getSelectedItem()
+                )*amount.getValue());
+    }
+
+    private double getRate(String from, String to) {
+        try {
+            URL url = new URL("https://free.currconv.com/api/v7/convert?q=" + from + "_" + to + "&compact=ultra&apiKey=a5788cc2decba3992574");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responsecode = conn.getResponseCode();
+
+            if (responsecode != 200) {
+                throw new RuntimeException("" + responsecode);
+            } else {
+                Scanner sc = new Scanner(url.openStream());
+                String inline = "";
+                while (sc.hasNext()) {
+                    inline += sc.nextLine();
+                }
+
+                String exchangeRateString = inline.substring(inline.indexOf(":") + 1, inline.indexOf("}"));
+                return Double.parseDouble(exchangeRateString);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
 }
