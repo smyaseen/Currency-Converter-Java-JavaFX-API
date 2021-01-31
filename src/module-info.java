2a091195c0719ea4070d7db8c2b3437c1c93427f
smy <smyaseen164@gmail.com>
2021-02-01 02:29:39 +0500

    all currencies added - need to automate

--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,7 @@ module Currency.Converter.Java.JavaFX.and.API {
     requires javafx.graphics;
     requires javafx.base;
     requires javafx.controls;
+    requires org.json;
     opens org.smy.currencyConverter.controller;
     opens org.smy.currencyConverter;
     opens org.smy.currencyConverter.view;
--- src/org/smy/currencyConverter/controller/Controller.java
+++ src/org/smy/currencyConverter/controller/Controller.java
@@ -29,12 +29,178 @@ public class Controller {
         ArrayList<String> currencyList = new ArrayList<>();
         currencyList.add("PKR");
         currencyList.add("USD");
+        currencyList.add("ALL");
+        currencyList.add("XCD");
+        currencyList.add("EUR");
+        currencyList.add("BBD");
+        currencyList.add("BTN");
+        currencyList.add("BND");
+        currencyList.add("XAF");
+        currencyList.add("CUP");
+        currencyList.add("FKP");
+        currencyList.add("GIP");
+        currencyList.add("HUF");
+        currencyList.add("IRR");
+        currencyList.add("JMD");
+        currencyList.add("AUD");
+        currencyList.add("LAK");
+        currencyList.add("LYD");
+        currencyList.add("MKD");
+        currencyList.add("XOF");
+        currencyList.add("NZD");
+        currencyList.add("OMR");
+        currencyList.add("PGK");
+        currencyList.add("RWF");
+        currencyList.add("WST");
+        currencyList.add("RSD");
+        currencyList.add("SEK");
+        currencyList.add("TZS");
+        currencyList.add("AMD");
+        currencyList.add("BSD");
+        currencyList.add("BAM");
+        currencyList.add("CVE");
+        currencyList.add("CNY");
+        currencyList.add("CRC");
+        currencyList.add("CZK");
+        currencyList.add("ERN");
+        currencyList.add("GEL");
+        currencyList.add("HTG");
+        currencyList.add("INR");
+        currencyList.add("JOD");
+        currencyList.add("KRW");
+        currencyList.add("LBP");
+        currencyList.add("MWK");
+        currencyList.add("MRO");
+        currencyList.add("MZN");
+        currencyList.add("ANG");
+        currencyList.add("PEN");
+        currencyList.add("QAR");
+        currencyList.add("STD");
+        currencyList.add("SLL");
+        currencyList.add("SOS");
+        currencyList.add("SDG");
+        currencyList.add("SYP");
+        currencyList.add("AOA");
+        currencyList.add("AWG");
+        currencyList.add("BHD");
+        currencyList.add("BZD");
+        currencyList.add("BWP");
+        currencyList.add("BIF");
+        currencyList.add("KYD");
+        currencyList.add("COP");
+        currencyList.add("DKK");
+        currencyList.add("GTQ");
+        currencyList.add("HNL");
+        currencyList.add("IDR");
+        currencyList.add("ILS");
+        currencyList.add("KZT");
+        currencyList.add("KWD");
+        currencyList.add("LSL");
+        currencyList.add("MYR");
+        currencyList.add("MUR");
+        currencyList.add("MNT");
+        currencyList.add("MMK");
+        currencyList.add("NGN");
+        currencyList.add("PAB");
+        currencyList.add("PHP");
+        currencyList.add("RON");
+        currencyList.add("SAR");
+        currencyList.add("SGD");
+        currencyList.add("ZAR");
+        currencyList.add("SRD");
+        currencyList.add("TWD");
+        currencyList.add("TOP");
+        currencyList.add("VEF");
+        currencyList.add("DZD");
+        currencyList.add("ARS");
+        currencyList.add("AZN");
+        currencyList.add("BYR");
+        currencyList.add("BOB");
+        currencyList.add("BGN");
+        currencyList.add("CAD");
+        currencyList.add("CLP");
+        currencyList.add("CDF");
+        currencyList.add("DOP");
+        currencyList.add("FJD");
+        currencyList.add("GMD");
+        currencyList.add("GYD");
+        currencyList.add("ISK");
+        currencyList.add("IQD");
+        currencyList.add("JPY");
+        currencyList.add("KPW");
+        currencyList.add("LVL");
+        currencyList.add("CHF");
+        currencyList.add("MGA");
+        currencyList.add("MDL");
+        currencyList.add("MAD");
+        currencyList.add("NPR");
+        currencyList.add("NIO");
+        currencyList.add("PYG");
+        currencyList.add("SHP");
+        currencyList.add("SCR");
+        currencyList.add("SBD");
+        currencyList.add("LKR");
+        currencyList.add("THB");
+        currencyList.add("TRY");
+        currencyList.add("AED");
+        currencyList.add("VUV");
+        currencyList.add("YER");
+        currencyList.add("AFN");
+        currencyList.add("BDT");
+        currencyList.add("BRL");
+        currencyList.add("KHR");
+        currencyList.add("KMF");
+        currencyList.add("HRK");
+        currencyList.add("DJF");
+        currencyList.add("EGP");
+        currencyList.add("ETB");
+        currencyList.add("XPF");
+        currencyList.add("GHS");
+        currencyList.add("GNF");
+        currencyList.add("HKD");
+        currencyList.add("XDR");
+        currencyList.add("KES");
+        currencyList.add("KGS");
+        currencyList.add("LRD");
+        currencyList.add("MOO");
+        currencyList.add("MVR");
+        currencyList.add("MXN");
+        currencyList.add("NOK");
+        currencyList.add("NAD");
+        currencyList.add("PLN");
+        currencyList.add("RUB");
+        currencyList.add("SZL");
+        currencyList.add("TJS");
+        currencyList.add("TTD");
+        currencyList.add("UGX");
+        currencyList.add("UYU");
+        currencyList.add("VND");
+        currencyList.add("TND");
+        currencyList.add("UAH");
+        currencyList.add("UZS");
+        currencyList.add("TMT");
+        currencyList.add("GBP");
+        currencyList.add("ZMW");
+        currencyList.add("BTC");
+        currencyList.add("BYN");
+        currencyList.add("BMD");
+        currencyList.add("GGP");
+        currencyList.add("CLF");
+        currencyList.add("CUC");
+        currencyList.add("IMP");
+        currencyList.add("JEP");
+        currencyList.add("SVC");
+        currencyList.add("ZMK");
+        currencyList.add("XAG");
+        currencyList.add("ZWL");
 
         convertFrom.getItems().addAll(currencyList);
         convertTo.getItems().addAll(currencyList);
 
         convertFrom.getSelectionModel().select("USD");
         convertTo.getSelectionModel().select("PKR");
+
+
 
         amount.getValueFactory().setValue(1);
 
@@ -77,8 +243,7 @@ public class Controller {
     private double getRate(String from, String to) {
         try {
             // get free api from https://free.currencyconverterapi.com/
-            // or any other
-            URL url = new URL("[YOUR API]");
+            URL url = new URL("https://free.currconv.com/api/v7/convert?q=" + from + "_" + to + "&compact=ultra&apiKey=a5788cc2decba3992574");
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
@@ -104,5 +269,7 @@ public class Controller {
         }
 
     }
+
+
 
 }
